499e1f60b6bf63d0dcc031a9f979fb17ecfc2280
jenny.precenth@chasacademy.se <jenny.precenth@chasacademy.se>
2025-10-07 11:18:55 +0200

    2nd Commit

--- q9.js
+++ q9.js
@@ -3,17 +3,29 @@
 export function myCalculator(operator, num1, num2) {
   switch (operator) {
     case "+":
-      return myAddFunction(num1, num2);
-
+    function myAddFunction(num1, num2) {
+      let sum = num1 + num2;
+      return sum;
+    }
+    myAddFunction(num1, num2);
     case "-":
-      return mySubtractFunction(num1, num2);
-
+    function mySubtractFunction(num1, num2) {
+      let sum = num1 - num2;
+      return sum;
+    }
+    mySubtractFunction(num1, num2);
     case "*":
-      return myMultiplyFunction(num1, num2);
-
+    function myMultiplyFunction(num1, num2) {
+      let sum = num1 * num2;
+      return sum;
+    }
+    myMultiplyFunction(num1, num2);
     case "/":
-      return myDivisionFunction(num1, num2);
-
+    function myDivisionFunction(num1, num2) {
+      let sum = num1 / num2;
+      return sum;
+    }
+    myDivisionFunction(num1, num2);
     default:
       return "Error operator not found!";
   }
